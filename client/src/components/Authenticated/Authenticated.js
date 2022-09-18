c993df8c6c49f7b491e6cd5761962f565cd10048
Nixon <limnixon03292001@gmail.com>
2022-09-18 13:38:21 +0800

    add notfoun page

--- client/src/components/Authenticated/Authenticated.js
+++ client/src/components/Authenticated/Authenticated.js
@@ -5,7 +5,7 @@ import Right from '../Homepage/components/Right'
 import Homepage from '../Homepage/Homepage'
 import Profile from '../Profile/Profile'
 import Search from '../Search/Search'
-
+import NotFound from '../404/NotFound'
 const Authenticated = () => {
   return (
     <div className="grid grid-cols-12  md:gap-5 max-w-6xl m-auto w-full text-white p-2">
@@ -16,6 +16,7 @@ const Authenticated = () => {
             <Route exact path="/" component={Homepage}/>
             <Route exact path="/profile/:id" component={Profile}/>
             <Route exact path="/search" component={Search}/>
+            <Route exact path="*" component={NotFound}/>
         </Switch>
 
         <Right/>
